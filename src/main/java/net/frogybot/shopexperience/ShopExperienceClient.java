2ee429fe5f2df14edfdd48ca2ffdd0df87d12bed
frogybot <edwardthomashanley@outlook.com>
2023-08-17 19:21:09 +0100

    Initial commit

---
+++ src/main/java/net/frogybot/shopexperience/ShopExperienceClient.java
@@ -0,0 +1,10 @@
+package net.frogybot.shopexperience;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShopExperienceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/frogybot/shopexperience/ShopExperienceModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.frogybot.shopexperience;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ShopExperienceModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
